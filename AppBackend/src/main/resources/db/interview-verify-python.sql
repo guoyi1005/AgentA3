8ff967cb95256b2627bbe228c6e31ed98ad98e94
LukeZhang <zzs@qq.com>
2026-09-10 15:08:33 +0800

    测试数据

---
+++ AppBackend/src/main/resources/db/interview-verify-python.sql
@@ -0,0 +1,4 @@
+﻿SELECT COUNT(*) AS python_count FROM knowledge_base WHERE job_position='Python 开发';
+SELECT question_type, COUNT(*) AS c FROM knowledge_base WHERE job_position='Python 开发' GROUP BY question_type;
+SELECT LEFT(question, 30) AS q, LEFT(remark, 50) AS src FROM knowledge_base WHERE job_position='Python 开发' LIMIT 3;
+SELECT COUNT(*) AS job_ok FROM interview_job_tech_stack WHERE job_position='Python 开发';
